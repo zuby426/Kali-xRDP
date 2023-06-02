15125bebd00d57d6a947ac2d85803869997262e1
zuby426 <135287426+zuby426@users.noreply.github.com>
2023-06-02 02:44:00 +0100

    Update Kali-xRDP.cmd

--- Kali-xRDP.cmd
+++ Kali-xRDP.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF & NET SESSION >NUL 2>&1 
 IF %ERRORLEVEL% == 0 (ECHO Administrator check passed...) ELSE (ECHO You need to run this command with administrative rights.  Is User Account Control enabled? && pause && goto ENDSCRIPT)
 COLOR 1F
-SET GITORG=DesktopECHO
+SET GITORG=ZubysKaliGUI
 SET GITPRJ=Kali-xRDP
 SET BRANCH=main
 SET BASE=https://github.com/%GITORG%/%GITPRJ%/raw/%BRANCH%
